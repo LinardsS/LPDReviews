ffb1223fb465e7821007691c082791aba4083104
LinardsS <67050806+LinardsS@users.noreply.github.com>
2020-06-22 17:09:28 +0300

    Update about.blade.php

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,7 +4,7 @@
 <br>
 <div class="container">
 <h1> <?php echo $title; ?></h1>
-<p>Here at LPDReviews we welcome people to share their thoughts about various movies, videogames and music albums! </p>
+<p>Here at LPDReviews we welcome everyone to share their thoughts about various movies, videogames and music titles! </p>
 </div>
 @endsection
 
