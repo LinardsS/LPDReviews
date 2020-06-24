2d40c982a942790a678f84eae97ee43e1a1eab25
LinardsS <67050806+LinardsS@users.noreply.github.com>
2020-06-24 23:14:02 +0300

    Update admin.blade.php

--- resources/views/pages/admin.blade.php
+++ resources/views/pages/admin.blade.php
@@ -21,10 +21,10 @@
         @foreach($users as $user)
             <tr>
                {{--  <form action="{{ route('admin.assign') }}" method="post"> --}}
-                    <td>{{ $user->name }}</td>
+                    {{-- <td>{{ $user->name }}</td>
                     <td>{{ $user->email }} <input type="hidden" name="email" value="{{ $user->email }}"></td>
                     <td><input type="checkbox" {{ $user->hasRole('User') ? 'checked' : '' }} name="role_user"></td>
-                    <td><input type="checkbox" {{ $user->hasRole('Admin') ? 'checked' : '' }} name="role_admin"></td>
+                    <td><input type="checkbox" {{ $user->hasRole('Admin') ? 'checked' : '' }} name="role_admin"></td> --}}
                     {{-- <td><input type="checkbox" {{ $user->hasRole('Author') ? 'checked' : '' }} name="role_author"></td> --}}
                     {{ csrf_field() }}
                     {{-- <td><button type="submit">Assign Roles</button></td> --}}
